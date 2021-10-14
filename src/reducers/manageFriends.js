d1ef47f4570e2e878d210de1d481483a596fb175
derek <derekle.creative@gmail.com>
2021-10-13 23:19:22 -0700

    commit tree 8d658c354befca0cfa812496cabc88b178130f41

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
 export function manageFriends(state, action){
+    state = state || {
+        friends: []
+    }
+    switch (action.type) {
+        case "ADD_FRIEND":
+            console.log('addFriend')
+            const friends = state.friends
+            const friend = action.friend
+            console.log(friends)
+            console.log({ friends: friends.push(friend) })
+        case 'REMOVE_FRIEND':
+            const filtered = state.friends.filter((friend) => friend.id != action.id)
+            return {friends: filtered}
+        default:
+            return state
+      }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,11 @@
 export function managePresents(state, action){
+    state = state || {
+        numberOfPresents: 0
+    }
+    return (
+        action.type === 'INCREASE'
+        ? {numberOfPresents: state.numberOfPresents + 1}
+        : state
+    )
+
 }
